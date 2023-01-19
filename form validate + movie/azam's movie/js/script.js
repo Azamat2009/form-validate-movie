dced038cc5b057ff91622b1c87e43a1910469024
Azamat <rakhmatov.azamat2009@gmail.com>
2023-01-19 21:10:03 +0500

    form validate + movie
    form validate + movie
    
    поставьте минимум 9

---
+++ form validate + movie/azam's movie/js/script.js
@@ -0,0 +1,41 @@
+/* Задания на урок:
+
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
+
+2) Изменить жанр фильма, поменять "комедия" на "драма"
+
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
+
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
+
+5) Добавить нумерацию выведенных фильмов */
+
+'use strict';
+
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим"
+    ]
+};
+
+let newTitle = document.querySelector('.promo__genre')
+newTitle.innerHTML = 'Драма'
+
+
+let advertisement = document.querySelectorAll('.promo__adv img');
+advertisement.forEach( (img) => {
+    img.remove()
+
+})
+
+let background = document.querySelector('.promo__bg')
+function newPhoto() {
+    background.style.backgroundImage = "url('./img/bg.jpg')"
+}
+newPhoto()
